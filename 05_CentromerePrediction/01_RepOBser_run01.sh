fc1324938d908826919db2b9b244d7cbc6c9c092
Laura Chiara Pettrich <100533414+lpettrich@users.noreply.github.com>
2024-10-03 15:04:50 +0200

    Add files via upload
    run01 -g FALSE -->  AT DNAwalk
    run 01 -g TRUE --> CG DNAwalk

---
+++ 05_CentromerePrediction/01_RepOBser_run01.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=128000M
+#SBATCH --cpus-per-task=15
+#SBATCH --time=05:00:00
+#SBATCH --account=ag-waldvogel
+#SBATCH --job-name=RepOBserver_run01
+#SBATCH --error /scratch/lpettric/jobs/%x-%N-%j.err
+#SBATCH --output /scratch/lpettric/jobs/%x-%N-%j.out
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=lpettric@smail.uni-koeln.de
+
+# Load modules
+module purge
+module load miniconda/py311_23.9.0
+conda activate /scratch/lpettric/conda/RepOBserver_env/
+
+# Set directory
+DIR=/projects/ag-waldvogel/pophistory/CRIP/centromeres/run01
+
+cd ${DIR}
+
+# Start RepeatOBserverV1
+srun Setup_Run_Repeats.sh -i Criparius -f Chironomus_riparius_genome_010921.fasta -h H0 -c 15 -m 128000 -g FALSE
+
